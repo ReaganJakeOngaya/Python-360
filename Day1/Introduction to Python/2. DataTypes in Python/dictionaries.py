96d5d06262050e09710e2088e1f0b64ca9616a5d
ReaganJakeOngaya <reaganjakeongaya@gmail.com>
2025-11-04 23:02:55 +0300

    Nested Dictionaries added

--- Day1/Introduction to Python/2. DataTypes in Python/dictionaries.py
+++ Day1/Introduction to Python/2. DataTypes in Python/dictionaries.py
@@ -24,5 +24,42 @@ print(my_dict)
 
 # Updating a dictionary value
 my_dict["age"] = 31
-update_dict = my_dict
-print(update_dict)
+updated_dict = my_dict
+print(updated_dict)
+print(my_dict)
+
+for key, value in sorted(my_dict.items()):
+    if user_public:
+        print(f"{key}: {value}")
+    else:
+        print("Data is private")
+
+# Removing a key-value pair
+del my_dict["city"]
+print(my_dict) 
+
+# Nested Dictionaries
+people_dict = {
+    "person1": {
+        "name": "Bob",
+        "age": 25,
+        "city": "Los Angeles",
+        "job": "Designer",
+        "skills": ["Photoshop", "Illustrator", "Figma"]
+    },
+    "person2": {
+        "name": "Carol",
+        "age": 28,
+        "city": "Chicago",
+        "job": "Manager",
+        "skills": ["Leadership", "Communication", "Strategic Planning"]
+    }
+}
+
+for person, details in people_dict.items():
+    print(f"\nDetails of {person}:")
+    for key, value in details.items():
+        if user_public:
+            print(f"{key}: {value}")
+        else:
+            print("Data is private") 
