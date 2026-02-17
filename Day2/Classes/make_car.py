c76c081de2be189eb991ce5087487ed56ea75122
ReaganJakeOngaya <reaganjakeongaya@gmail.com>
2026-02-17 21:06:42 +0300

    class modules

--- Day2/Classes/make_car.py
+++ Day2/Classes/make_car.py
@@ -1,2 +1,10 @@
+from Car import Car, ElectricCar
 
+my_new_car = Car("Volvo", "TX4", 2020)
+print(my_new_car.get_descriptive_name())
 
+my_new_car.odometer_reading = 33
+my_new_car.read_odometer()
+
+my_electrict_car = ElectricCar("Tesla", "Model S", 2022)
+print(my_electrict_car.get_descriptive_name())
