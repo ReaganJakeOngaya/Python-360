f1a407ce8b2678324b54ef8ab009ffb05009af3c
ReaganJakeOngaya <reaganjakeongaya@gmail.com>
2026-03-30 14:44:40 +0300

    type conversion

---
+++ Day4/conversion.py
@@ -0,0 +1,10 @@
+#in python, we can convert between different data types using 
+# built-in functions like int(), float(), str(), etc.
+
+int = 123
+float = 1.23
+
+new_int = int + float
+
+print(new_int)
+print(type(new_int))
