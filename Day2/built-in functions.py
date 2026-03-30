86c67dad5ec0315676e028ca3e8059c55e3471f3
ReaganJakeOngaya <reaganjakeongaya@gmail.com>
2026-03-30 15:14:40 +0300

    built-in functions

---
+++ Day2/built-in functions.py
@@ -0,0 +1,27 @@
+# Python has many built-in functions that are available for use without needing to import any modules. 
+# These functions perform various tasks and can be used to manipulate data, perform calculations, and more.
+# for more built-in functions, you can refer to the official Python documentation: 
+# https://docs.python.org/3/library/functions.html
+# Some common built-in functions include:
+# - print(): Outputs text to the console
+print("Hello, World!"
+      )
+# - len(): Returns the length of an object
+my_list = [1, 2, 3, 4, 5]
+print(len(my_list))
+
+# - type(): Returns the type of an object
+print(type(my_list))
+# - int(), float(), str(): Converts values to different data types
+number = "123"
+converted_number = int(number)
+print(converted_number)
+print(type(converted_number))
+
+# - max(), min(): Returns the largest or smallest item in an iterable
+numbers = [10, 20, 5, 15]
+print(max(numbers))
+print(min(numbers))
+
+# - sum(): Returns the sum of all items in an iterable
+print(sum(numbers))
